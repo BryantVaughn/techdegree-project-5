ab34026e8ec0a3f4124028a1307d96620ef415fa
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-10-23 14:27:33 -0600

    Add custom search logic

--- js/customSearch.js
+++ js/customSearch.js
@@ -1,1 +1,20 @@
-const input = document.querySelector('.search');
+const searchInput = document.querySelector('.search');
+const imgGallery = document.querySelectorAll('.gallery a');
+
+const filterImages = (evt) => {
+	// check event to see if backspace
+
+	// use previous search state with new key added
+	let searchVal = evt.target.value;
+
+	for (let i = 0; i < imgGallery.length; i++) {
+		const desc = imgGallery[i].getAttribute('data-caption');
+		if (desc.toLowerCase().includes(searchVal.toLowerCase())) {
+			imgGallery[i].classList.remove('hidden');
+		} else {
+			imgGallery[i].classList.add('hidden');
+		}
+	}
+};
+
+searchInput.addEventListener('keyup', filterImages);
